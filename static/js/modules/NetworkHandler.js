2ba03bd58c4b75afa8ac1f1b0debc5bcc9d7f583
Gel0bmstu <leshikne@gmail.com>
2019-04-15 04:22:30 +0300

    fix

--- static/js/modules/NetworkHandler.js
+++ static/js/modules/NetworkHandler.js
@@ -46,12 +46,7 @@ export default class NetworkHandler {
 				callback(data);
 			})
 			.catch((error) => {
-<<<<<<< HEAD
 				callback();
-=======
-				console.log(error);
-				callback(response.status);
->>>>>>> newBuilder
 			})
 	}
 
