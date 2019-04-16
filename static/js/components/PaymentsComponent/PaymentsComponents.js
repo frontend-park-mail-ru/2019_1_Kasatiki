5c36b443db46fc7a8ddfa2fad8ebecc35c7f639f
Gel0bmstu <leshikne@gmail.com>
2019-04-16 15:09:47 +0300

    payout added

--- static/js/components/PaymentsComponent/PaymentsComponents.js
+++ static/js/components/PaymentsComponent/PaymentsComponents.js
@@ -9,26 +9,27 @@ export default class PaymentsComponent {
 			templateScript = `
                 <div class="payments">
                     <h1 class="payments__title">Payments</h1>
-                    <form class="payments__input-section" action="/payments" method="post" id="contact_form">
-                        <input type="text" name="phone" class="payments__phonenumber" placeholder="9xxxxxxxxx">
-                        <input type="text" name="amout" placeholder="100 $">
+                    <form class="payments__input-section" onsubmit="this.send()" method="post" id="contact_form" required=""> 
+                        <input class="payments__input-section-input" type="text" name="phone" class="payments__phonenumber" placeholder="9xxxxxxxxx" required="">
+                        <input class="payments__input-section-input" type="text" name="amount" placeholder="100 $">
                         <button href="/"><i class="fas fa-angle-left"></i></button>
-                        <button type="submit" id="form_button">Payout</button>
+                        <button href="/payout" class="payments__input-section-submit" type="submit" id="form_button">Submit</button>
                     </form>
                 </div>
             `;
 		} else {
 			templateScript = `
                 <div class="chose">
-                    <h1 class="chose__title">ADVHATER</h1>
+                    <h1 class="chose__title">Payment</h1>
                     <div class="main-bar">
-                        <button href="/signup" class="main-bar__item chose_btn" data-title="SIGNUP" data-section="signup"><i href="/signup" class="fas fa-user-plus"></i></button>
-                        <button href="/login" class="main-bar__item chose_btn" data-title="LOGIN" data-section="login"><i href="/login" class="fas fa-sign-in-alt"></i></button>
+                        <button href="/signup" class="main-bar__item chose_btn_pay" data-title="SIGNUP" data-section="signup"><i href="/signup" class="fas fa-user-plus"></i></button>
+                        <button href="/login" class="main-bar__item chose_btn_pay" data-title="LOGIN" data-section="login"><i href="/login" class="fas fa-sign-in-alt"></i></button>
                     </div> 
+                    <h3 class="chose__title">login or signup please</h3>
                 </div>
 			`;
 		}
 		const template = Handlebars.compile(templateScript);		
         return template();
-	}
+    }
 }
--- static/js/views/PaymentsView.js
+++ static/js/views/PaymentsView.js
@@ -18,9 +18,36 @@ export default class PaymentsView extends BaseView {
                 if (typeof(data) === 'object') {
                     console.log("IM IN",data.status);
                     that.root.innerHTML = that.PaymentsComponent.render(true);
+                } else {
+                    that.root.innerHTML = that.PaymentsComponent.render(true);
                 }
+                that.initSpecialRoutes();
             },
             path: '/api/isauth',
+        });
+    }
+
+    initSpecialRoutes() {
+        this.specialRoutes['/payout'] = this.send;
+    }
+
+    send() {
+        const form = document.querySelector('.payments__input-section');
+
+        let payload = {
+            phone : form.phone.value,
+            amount : form.amount.value,
+        }
+
+        NetworkHandler.doPost({
+            callback(data) {
+                console.log('Success:',data);
+                if (data === 201) {
+                    that.router.go('/');
+                }
+            },
+            path: '/api/payments',
+            body: JSON.stringify( payload ),
         });
     }
 }
